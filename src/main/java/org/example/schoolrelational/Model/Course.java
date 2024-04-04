6ccfcc417a75f64e864def4d0af37166c37ede86
Haya <154345101+1Haya1@users.noreply.github.com>
2024-04-04 13:53:02 +0300

    Add files via upload

---
+++ src/main/java/org/example/schoolrelational/Model/Course.java
@@ -0,0 +1,32 @@
+package org.example.schoolrelational.Model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class Course {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+
+    @Column(columnDefinition = "varchar(20) not null")
+    @NotNull(message = "cant be null")
+    private String name;
+
+
+    @ManyToOne
+    @JoinColumn(name = "teacher_id",referencedColumnName = "id")
+    @JsonIgnore
+    private Teacher teacher;
+}
